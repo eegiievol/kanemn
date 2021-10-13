e546469d8d8bc7df509fd1ae28e6aef0a9429407
unknown <eulziisaikhan@miu.edu>
2021-10-13 18:35:58 -0500

    aa233345366t5

--- w3d3/maze.js
+++ w3d3/maze.js
@@ -1,20 +1,30 @@
 $(()=>{
     //let flag=False;
     $("#start").on('click', gameStarts);
-    $("#end").on('click', win);
+    $("#end").on('mouseenter', win);
 });
 
+var ended = false;
 function lose(evt) {
-    $(".boundary").css({"background-color": "red"});
-    alert("YOU HAVE LOST");
-    return false;
+    if (!ended) {
+        $(".boundary").css({"background-color": "red"});
+        $("#status").text("YOU HAVE LOST");
+        ended = true;
+    }
 }
 
-let win = () => alert("you have win, congratz!!");    
+let win = () => {
+    if (!ended) {
+        $("#status").text("You win, congratz");
+        ended = true;
+    }     
+}   
 
 function gameStarts(){
-    console.log("game started, have fun!!");
+    $("#status").text("Click the \"S\" to begin.");
+    $(".boundary").css({"background-color": "#eeeeee"});
     $(".boundary").on('mouseover', lose);
     $("#maze").on('mouseleave', lose);
+    ended = false;
 }
 
